0630db0a8e0ee5a9e465fb9881ff06505a1f290b
justinhoyt <justinhoyt@gmail.com>
2024-12-15 11:13:58 +0000

    Refactor Queue to no longer extend Array

--- queue.ts
+++ queue.ts
@@ -1,26 +1,35 @@
-export class Queue<T> extends Array<T> {
+export class Queue<T> {
   private offset = 0;
+  private arr: T[] = [];
+
+  constructor(...vals: T[]) {
+    this.arr.push(...vals);
+  }
 
   get size() {
-    return this.length - this.offset;
+    return this.arr.length - this.offset;
   }
 
   isEmpty() {
     return this.size === 0;
   }
 
+  peek() {
+    return this.arr[0];
+  }
+
   enqueue(val: T) {
-    this.push(val);
+    this.arr.push(val);
   }
 
   dequeue() {
     if (this.size === 0) return;
 
-    if (this.offset > this.length / 2) {
-      this.splice(0, this.offset);
+    if (this.offset > this.arr.length / 2) {
+      this.arr.splice(0, this.offset);
       this.offset = 0;
     }
 
-    return this[this.offset++];
+    return this.arr[this.offset++];
   }
 }
--- queue_test.ts
+++ queue_test.ts
@@ -3,7 +3,7 @@ import { assertEquals } from "@std/assert";
 
 Deno.test("should insert and remove ordered list in order", () => {
   const queue = new Queue(1, 2, 3, 4, 5);
-  assertEquals(queue.length, 5);
+  assertEquals(queue.size, 5);
 
   const actual: number[] = [];
   while (!queue.isEmpty()) {
