f7f56a4865496ad444b0f916d57abf64910df9cf
justinhoyt <justinhoyt@gmail.com>
2024-12-15 10:58:49 +0000

    Refactor the default object code into the tests

--- default_map.ts
+++ default_map.ts
@@ -7,22 +7,3 @@ export class DefaultMap<K, V> extends Map<K, V> {
     return super.get(key) ?? super.set(key, this.init()).get(key);
   }
 }
-
-// Default map behavior can also be achieved with plain objects
-if (import.meta.main) {
-  const arr = [1, 2, 3, 3, 4, 5, 5, 5, 5, 6, 6, 6, 6, 6, 7, 7];
-
-  // Use the nullish coalescing operator for primitives
-  const idCounts: Record<string, number> = {};
-  for (const num of arr.values()) {
-    idCounts[num] = (idCounts[num] ?? 0) + 1;
-  }
-  console.log(idCounts);
-
-  // Use the nullish coalescing assignment operator for objects
-  const idLocations: Record<string, number[]> = {};
-  for (const [i, num] of arr.entries()) {
-    (idLocations[num] ??= []).push(i);
-  }
-  console.log(idLocations);
-}
--- default_map_test.ts
+++ default_map_test.ts
@@ -24,3 +24,26 @@ Deno.test("should insert and update object values", () => {
   assertEquals(idLocations.get(6), [9, 10, 11, 12, 13]);
   assertEquals(idLocations.get(100), []);
 });
+
+// Default map behavior can also be achieved with plain objects
+Deno.test("Plain object: should default for primitives", () => {
+  const arr = [1, 2, 3, 3, 4, 5, 5, 5, 5, 6, 6, 6, 6, 6, 7, 7];
+
+  // Use the nullish coalescing operator for primitives
+  const idCounts: Record<string, number> = {};
+  for (const num of arr.values()) {
+    idCounts[num] = (idCounts[num] ?? 0) + 1;
+  }
+  assertEquals(idCounts[6], 5);
+});
+
+Deno.test("Plain object: should default for objects", () => {
+  const arr = [1, 2, 3, 3, 4, 5, 5, 5, 5, 6, 6, 6, 6, 6, 7, 7];
+
+  // Use the nullish coalescing assignment operator for objects
+  const idLocations: Record<string, number[]> = {};
+  for (const [i, num] of arr.entries()) {
+    (idLocations[num] ??= []).push(i);
+  }
+  assertEquals(idLocations[3], [2, 3]);
+});
