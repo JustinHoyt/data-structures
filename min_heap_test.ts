2ea5e1f7cff26c9fea0e31b409fd7f6658feef3c
justinhoyt <justinhoyt@gmail.com>
2024-12-15 10:36:29 +0000

    Add example of deno's BinaryHeap

--- min_heap_test.ts
+++ min_heap_test.ts
@@ -1,31 +1,8 @@
 import { MinHeap } from "./min_heap.ts";
 import { assertEquals } from "@std/assert";
+import { ascend, BinaryHeap } from "jsr:@std/data-structures";
 
-Deno.test("should insert and remove ordered list in order", () => {
-  const heap = new MinHeap([1, 2, 3, 4, 5]);
-
-  const actual = [];
-  let next: number | undefined = 0;
-  while ((next = heap.pop()) != null) {
-    actual.push(next);
-  }
-
-  assertEquals(actual, [1, 2, 3, 4, 5]);
-});
-
-Deno.test("should insert and remove descending ordered list in ascending order", () => {
-  const heap = new MinHeap([5, 4, 3, 2, 1]);
-
-  const actual = [];
-  let next: number | undefined = 0;
-  while ((next = heap.pop()) != null) {
-    actual.push(next);
-  }
-
-  assertEquals(actual, [1, 2, 3, 4, 5]);
-});
-
-Deno.test("should handle duplicates", () => {
+Deno.test("MinHeap: should handle duplicates", () => {
   const heap = new MinHeap([3, 5, 4, 4, 3, 3, 2, 1, 2]);
 
   const actual = [];
@@ -37,20 +14,20 @@ Deno.test("should handle duplicates", () => {
   assertEquals(actual, [1, 2, 2, 3, 3, 3, 4, 4, 5]);
 });
 
-Deno.test("should handle single entry", () => {
+Deno.test("MinHeap: should handle single entry", () => {
   const heap = new MinHeap([3]);
 
   assertEquals(heap.pop(), 3);
   assertEquals(heap.length, 0);
 });
 
-Deno.test("should do nothing on empty array", () => {
+Deno.test("MinHeap: should do nothing on empty array", () => {
   const heap = new MinHeap([]);
 
   assertEquals(heap.length, 0);
 });
 
-Deno.test("should handle large heap", () => {
+Deno.test("MinHeap: should handle large heap", () => {
   const testArr = Array<number>(100_000).fill(0).map(
     () => Math.floor(Math.random() * 100_000),
   );
@@ -64,4 +41,17 @@ Deno.test("should handle large heap", () => {
   }
 
   assertEquals(actual, expected);
+});
+
+// Deno's std library also has a binary heap
+Deno.test("BinaryHeap: should insert and remove descending ordered list in ascending order", () => {
+  const heap = new BinaryHeap<number>(ascend);
+  heap.push(5, 4, 3, 2, 1);
+
+  const actual = [];
+  while (!heap.isEmpty()) {
+    actual.push(heap.pop());
+  }
+
+  assertEquals(actual, [1, 2, 3, 4, 5]);
 });
